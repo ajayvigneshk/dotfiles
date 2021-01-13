06b4b28fa6aec09d32b55c887407f5dd5fcf44f5
Ajay Vignesh K <ajayvignesh.k@gmail.com>
2021-01-13 13:26:35 +0530

    Add brew install(minimal, all) scripts

---
+++ .config/requirements-all.sh
@@ -0,0 +1,2 @@
+brew install --cask firefox vscodium docker spotify
+brew install stretchly docker
---
+++ .config/requirements-minimal.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+brew install git fzf ripgrep tmux neovim jq nnn
+#Interactive
+$(brew --prefix)/opt/fzf/install
+brew install --cask alacritty spectacle homebrew/cask-fonts/font-jetbrains-mono
+brew install gpg pinentry-mac
