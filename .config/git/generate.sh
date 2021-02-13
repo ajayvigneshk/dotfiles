f39da32f051d1fb950fb97391143d04ee64f12de
Ajay Vignesh K <ajayvignesh.k@gmail.com>
2021-02-13 14:19:18 +0530

    [git] Version control personal git dotfiles

--- .config/git/generate.sh
+++ .config/git/generate.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# Used to set sensitive information on specific git config files
 GIT_CONFIG_DIR="$HOME/.config/git"
 #decrypt once
 jsonString=$(gpg -q --decrypt $HOME/.config/git/gitconfig.json.gpg)
@@ -6,14 +7,10 @@ max=$(jq '. | length' <<< $jsonString)
 i=0
 while [ $i -lt $max ]
 do
-	gitDir=$(jq -r ".[$i].gitDir" <<< $jsonString)
-	configName=$(jq -r ".[$i].configName" <<< $jsonString)
+	configFileName=$(jq -r ".[$i].configFileName" <<< $jsonString)
 	userName=$(jq -r ".[$i].userName" <<< $jsonString)
 	email=$(jq -r ".[$i].email" <<< $jsonString)
-	sshCommand=$(jq -r ".[$i].sshCommand" <<< $jsonString)
-	git config --file="$GIT_CONFIG_DIR/config" "includeif.$gitDir.path" $GIT_CONFIG_DIR/config-$configName
-	git config --file="$GIT_CONFIG_DIR/config-$configName" user.name "$userName"
-	git config --file="$GIT_CONFIG_DIR/config-$configName" user.email $email
-	git config --file="$GIT_CONFIG_DIR/config-$configName" core.sshCommand "$sshCommand"
+	git config --file="$GIT_CONFIG_DIR/$configFileName" user.name "$userName"
+	git config --file="$GIT_CONFIG_DIR/$configFileName" user.email $email
 	true $(( i++ ))
 done
