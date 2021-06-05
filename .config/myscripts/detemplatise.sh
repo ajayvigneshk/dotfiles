43ab1f8cb9589f60b711bef5fc29c482197d56a7
Ajay Vignesh K <ajayvignesh.k@gmail.com>
2021-06-05 20:26:59 +0530

    [alacritty] cleanup alacritty based config
    alacritty.yml is close to stock(only imports)
    separate dirs on same level with modular configs that can be imported
    Also took time to make emacs work with alacritty(keybindings for meta)

--- .config/myscripts/detemplatise.sh
+++ .config/myscripts/detemplatise.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sed "s/@@background@@/$1/g" ~/.config/nvim/init.template.vim > ~/.config/nvim/init.vim
-sed "s/@@background@@/$1/g" ~/.config/alacritty/alacritty.template.yml > ~/.config/alacritty/alacritty.yml
+sed "s/@@background@@/$1/g" ~/.config/alacritty/custom/colorschemes.template.yml > ~/.config/alacritty/custom/colorschemes.yml
 sed "s/@@background@@/$1/g" ~/.tmux.template.conf > ~/.tmux.conf
 tmux source-file ~/.tmux.conf 
 if [ "$2" == "presentation" ]; then
